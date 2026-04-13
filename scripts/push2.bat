091d72872e42a5b1d6d0a7a2b63c512ffa862f2b
pigfarm1 <bytspot@gmail.com>
2026-04-13 07:07:02 -0400

    chore: cleanup stray files

--- scripts/push2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d c:\Users\godmo\.augment\bytspot-beta
-git add -A
-git commit -m "legal: Add Disclaimer page + /disclaimer route + legal links in Profile"
-git push --no-verify origin master:main 2>&1
-echo DONE
